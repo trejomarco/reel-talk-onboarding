537671ab474a7098330ee28c2c5218eb939c4be0
marco-28-1994 <marco.t1994@gmail.com>
2023-10-27 13:51:07 -0500

    updating logos

--- src/components/UserDetailsForm.tsx
+++ src/components/UserDetailsForm.tsx
@@ -1,16 +1,16 @@
-import { Point } from 'react-easy-crop';
 import React, { useState, useCallback } from 'react';
 import Cropper from 'react-easy-crop';
 import styles from './UserDetailsForm.module.css';
+import ReelOrangeIcon from '../assets/reel-orange-icon.svg'; 
 
 const UserDetailsForm: React.FC = () => {
     const [displayName, setDisplayName] = useState('');
     const [location, setLocation] = useState('');
     const [imageSrc, setImageSrc] = useState<string | null>(null);
-    const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
-    const [zoom, setZoom] = useState(1);
+    const [crop, setCrop] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState<number>(1);
 
-    const onCropChange = useCallback((newCrop: Point) => {
+    const onCropChange = useCallback((newCrop: { x: number, y: number }) => {
         setCrop(newCrop);
     }, []);
 
@@ -31,26 +31,36 @@ const UserDetailsForm: React.FC = () => {
 
     return (
         <div className={styles["user-details-form"]}>
-            <div className={styles["image-cropper-container"]}>
-                {imageSrc ? (
-                    <Cropper
-                        image={imageSrc}
-                        crop={crop}
-                        zoom={zoom}
-                        aspect={1}
-                        onCropChange={onCropChange}
-                        onZoomChange={onZoomChange}
+
+<img src={ReelOrangeIcon} className={styles.reelIcon} alt="Reel Icon" />
+
+
+            <div className={styles["image-section"]}>
+                <div className={styles["image-cropper-container"]}>
+                    {imageSrc ? (
+                        <Cropper
+                            image={imageSrc}
+                            crop={crop}
+                            zoom={zoom}
+                            aspect={1}
+                            onCropChange={onCropChange}
+                            onZoomChange={onZoomChange}
+                        />
+                    ) : (
+                        <div className={styles["image-placeholder"]}>
+                            Your picture goes here
+                        </div>
+                    )}
+                </div>
+                <label className={styles['upload-button']}>
+                    Upload Image
+                    <input
+                        type="file"
+                        onChange={handleImageChange}
+                        style={{ display: 'none' }}
                     />
-                ) : (
-                    <div className={styles["image-placeholder"]}>
-                        Your picture goes here
-                    </div>
-                )}
+                </label>
             </div>
-            <input
-                type="file"
-                onChange={handleImageChange}
-            />
             <input
                 className={styles.textfield}
                 type="text"
@@ -70,6 +80,539 @@ const UserDetailsForm: React.FC = () => {
 };
 
 export default UserDetailsForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useCallback } from 'react';
+// import Cropper from 'react-easy-crop';
+// import styles from './UserDetailsForm.module.css';
+
+// const UserDetailsForm: React.FC = () => {
+//     const [displayName, setDisplayName] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [imageSrc, setImageSrc] = useState<string | null>(null);
+//     const [crop, setCrop] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+//     const [zoom, setZoom] = useState<number>(1);
+
+//     const onCropChange = useCallback((newCrop: { x: number, y: number }) => {
+//         setCrop(newCrop);
+//     }, []);
+
+//     const onZoomChange = useCallback((newZoom: number) => {
+//         setZoom(newZoom);
+//     }, []);
+
+//     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//         const file = e.target.files![0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onloadend = () => {
+//                 setImageSrc(reader.result as string);
+//             };
+//         }
+//     };
+
+
+   
+
+
+//         return (
+//             <div className={styles["user-details-form"]}>
+//                 <div className={styles["image-cropper-container"]}>
+//                     {imageSrc ? (
+//                         <Cropper
+//                             image={imageSrc}
+//                             crop={crop}
+//                             zoom={zoom}
+//                             aspect={1}
+//                             onCropChange={onCropChange}
+//                             onZoomChange={onZoomChange}
+//                         />
+//                     ) : (
+//                         <div className={styles["image-placeholder"]}>
+//                             Your picture goes here
+//                         </div>
+//                     )}
+//                 </div>
+
+//                 <label className={styles['upload-button']}>
+//                     Upload Image
+//                     <input
+//                         type="file"
+//                         onChange={handleImageChange}
+//                         style={{ display: 'none' }}
+//                     />
+//                 </label>
+
+//                 <input
+//                     className={styles.textfield}
+//                     type="text"
+//                     placeholder="Display Name"
+//                     value={displayName}
+//                     onChange={(e) => setDisplayName(e.target.value)}
+//                 />
+//                 <input
+//                     className={styles.textfield}
+//                     type="text"
+//                     placeholder="Location"
+//                     value={location}
+//                     onChange={(e) => setLocation(e.target.value)}
+//                 />
+//             </div>
+//         );
+//     };
+
+//     export default UserDetailsForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useCallback } from 'react';
+// import Cropper from 'react-easy-crop';
+// import styles from './UserDetailsForm.module.css';
+
+// const UserDetailsForm: React.FC = () => {
+//     const [displayName, setDisplayName] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [imageSrc, setImageSrc] = useState<string | null>(null);
+//     const [crop, setCrop] = useState({ x: 0, y: 0 });
+//     const [zoom, setZoom] = useState(1);
+
+//     const onCropChange = useCallback((newCrop: Point) => {
+//         setCrop(newCrop);
+//     }, []);
+
+//     const onZoomChange = useCallback((newZoom: number) => {
+//         setZoom(newZoom);
+//     }, []);
+
+//     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//         const file = e.target.files![0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onloadend = () => {
+//                 setImageSrc(reader.result as string);
+//             };
+//         }
+//     };
+
+//     return (
+//         <div className={styles.container}>
+
+//             <div className={styles.header}>
+//                 <img src="/assets/reel-talk-orange-logo.png" alt="Reel Talk Logo" />
+//                 <span className={styles["header-text"]}>Reel Talk Onboarding</span>
+//             </div>
+
+//             <div className={styles["image-cropper-container"]}>
+//                 {imageSrc ? (
+//                     <Cropper
+//                         image={imageSrc}
+//                         crop={crop}
+//                         zoom={zoom}
+//                         aspect={1}
+//                         onCropChange={onCropChange}
+//                         onZoomChange={onZoomChange}
+//                     />
+//                 ) : (
+//                     <div className={styles["image-placeholder"]}>
+//                         Your picture goes here
+//                     </div>
+//                 )}
+//             </div>
+
+//             <label className={styles["upload-button"]}>
+//                 Upload Image
+//                 <input
+//                     type="file"
+//                     onChange={handleImageChange}
+//                     style={{ display: 'none' }}
+//                 />
+//             </label>
+
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Display Name"
+//                 value={displayName}
+//                 onChange={(e) => setDisplayName(e.target.value)}
+//             />
+
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Location"
+//                 value={location}
+//                 onChange={(e) => setLocation(e.target.value)}
+//             />
+
+//         </div>
+//     );
+// };
+
+// export default UserDetailsForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useCallback } from 'react';
+// import Cropper from 'react-easy-crop';
+// import styles from './UserDetailsForm.module.css';
+
+// const UserDetailsForm: React.FC = () => {
+//     const [displayName, setDisplayName] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [imageSrc, setImageSrc] = useState<string | null>(null);
+//     const [crop, setCrop] = useState({ x: 0, y: 0 });
+//     const [zoom, setZoom] = useState(1);
+
+//     const onCropChange = useCallback((newCrop: { x: number, y: number }) => {
+//         setCrop(newCrop);
+//     }, []);
+
+//     const onZoomChange = useCallback((newZoom: number) => {
+//         setZoom(newZoom);
+//     }, []);
+
+//     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//         const file = e.target.files![0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onloadend = () => {
+//                 setImageSrc(reader.result as string);
+//             };
+//         }
+//     };
+
+//     return (
+//         <div className={styles["user-details-form"]}>
+//             <div className={styles["image-cropper-container"]}>
+//                 {imageSrc ? (
+//                     <Cropper
+//                         image={imageSrc}
+//                         crop={crop}
+//                         zoom={zoom}
+//                         aspect={1}
+//                         onCropChange={onCropChange}
+//                         onZoomChange={onZoomChange}
+//                     />
+//                 ) : (
+//                     <div className={styles["image-placeholder"]}>
+//                         Your picture goes here
+//                     </div>
+//                 )}
+//             </div>
+
+//             <label className={styles.upload-button}>
+//                 Upload Image
+//                 <input
+//                     type="file"
+//                     onChange={handleImageChange}
+//                     style={{display: 'none'}}
+//                 />
+//             </label>
+
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Display Name"
+//                 value={displayName}
+//                 onChange={(e) => setDisplayName(e.target.value)}
+//             />
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Location"
+//                 value={location}
+//                 onChange={(e) => setLocation(e.target.value)}
+//             />
+//         </div>
+//     );
+// };
+
+// export default UserDetailsForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Point } from 'react-easy-crop';
+// import React, { useState, useCallback } from 'react';
+// import Cropper from 'react-easy-crop';
+// import styles from './UserDetailsForm.module.css';
+
+// const UserDetailsForm: React.FC = () => {
+//     const [displayName, setDisplayName] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [imageSrc, setImageSrc] = useState<string | null>(null);
+//     const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
+//     const [zoom, setZoom] = useState(1);
+
+//     const onCropChange = useCallback((newCrop: Point) => {
+//         setCrop(newCrop);
+//     }, []);
+
+//     const onZoomChange = useCallback((newZoom: number) => {
+//         setZoom(newZoom);
+//     }, []);
+
+//     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//         const file = e.target.files![0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.readAsDataURL(file);
+//             reader.onloadend = () => {
+//                 setImageSrc(reader.result as string);
+//             };
+//         }
+//     };
+
+//     return (
+//         <div className={styles["user-details-form"]}>
+//             <div className={styles["image-cropper-container"]}>
+//                 {imageSrc ? (
+//                     <Cropper
+//                         image={imageSrc}
+//                         crop={crop}
+//                         zoom={zoom}
+//                         aspect={1}
+//                         onCropChange={onCropChange}
+//                         onZoomChange={onZoomChange}
+//                     />
+//                 ) : (
+//                     <div className={styles["image-placeholder"]}>
+//                         Your picture goes here
+//                     </div>
+//                 )}
+//             </div>
+
+
+
+
+
+
+
+
+
+
+//             <input
+//                 type="file"
+//                 onChange={handleImageChange}
+//             />
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Display Name"
+//                 value={displayName}
+//                 onChange={(e) => setDisplayName(e.target.value)}
+//             />
+//             <input
+//                 className={styles.textfield}
+//                 type="text"
+//                 placeholder="Location"
+//                 value={location}
+//                 onChange={(e) => setLocation(e.target.value)}
+//             />
+//         </div>
+//     );
+// };
+
+// export default UserDetailsForm;
 
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,61 @@
+import React from 'react';
 import UserDetailsForm from '../components/UserDetailsForm';
+import styles from '../styles/Home.module.css'; // Ensure the path is correct
+
+
+
+
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Reel Talk Onboarding</h1>
+    <div className={styles.container}>
+      <h1 className={styles.onboardingTitle}>Reel Talk Onboarding</h1>
       <UserDetailsForm />
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function Home() {
+//   return (
+//     <div className={styles.main}>
+//       <h1 className={styles.onboardingTitle}>Reel Talk Onboarding</h1>
+//       <UserDetailsForm />
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import UserDetailsForm from '../components/UserDetailsForm';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Reel Talk Onboarding</h1>
+//       <UserDetailsForm />
+//     </div>
+//   );
+// }
